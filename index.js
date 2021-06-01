e8cee37e93b80e6ada24b318b25d1d0cdae15543
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-01 18:13:53 -0400

    delete keys

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // SPOTIFY SEARCH FUNCTIONS
-const client_ID = 'badf019474334b82a55c72dbb75f3739'
-const client_secret = '9dc25589bf2f4c9e9d2e30edbc9b1556'
-const token = 'BQBOo0m77_ll84bOWYXAo2w0u5MAZA55wLLGRPvwupR_uu_zjqh5vI6zGziWGJ82F0Tu4vaG4JY-VANB4uswocpoqU30RtRTL1fMLD4ITudK4t5NJRd89_0d5E3sGyxUJuUdAM-y8o6X9eubUarqcBSDXXUgXrMq7XBZFFQ-UodLJX_itBuhiFJedo81M4RdBg1B_bQdzRZExthUVpFxNdjQMXgLLxiOlsm0A2uuL7Vzlpbovm6l1JEI9_HmG05_Vsf8cyN_lJGfF72nvTZ-S68'
+const client_ID = ''
+const client_secret = ''
+const token = ''
 
 function searchSpotify(search) {
     fetch(`https://api.spotify.com/v1/search?q=${search}&type=track&limit=10`, {
